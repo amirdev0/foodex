8780fc7b9db2a728f1be39e9f6080878e3ac7242
amirdev0 <amigoabdur@gmail.com>
2024-03-25 04:06:00 +0500

    Fix user type in account api
    Replaced non-existing foodex data type

--- src/client/account_api.c
+++ src/client/account_api.c
@@ -6,6 +6,6 @@ extern int server_sock;
 
 int user_auth(string phone, string password);
 
-struct foodex_user_t user_get_info(int user_id);
+struct user_t user_get_info(int user_id);
 
-int user_set_info(struct foodex_user_t user);
+int user_set_info(struct user_t user);
--- src/client/account_api.h
+++ src/client/account_api.h
@@ -1,9 +1,23 @@
 #ifndef ACCOUNT_API_H
 #define ACCOUNT_API_H
 
+struct user_t {
+	int id;
+	enum user_type_e {
+		Customer,
+		Dasher,
+		Restaurant
+	} type;
+	string phone;
+	string name;
+	string email;
+	string address;
+	path image;
+};
+
 int user_auth(string phone, string password);
 
-struct foodex_user_t user_get_info(int user_id);
-int user_set_info(struct foodex_user_t user);
+struct user_t user_get_info(int user_id);
+int user_set_info(struct user_t user);
 
 #endif //ACCOUNT_API_H
