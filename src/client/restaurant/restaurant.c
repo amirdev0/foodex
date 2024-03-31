146bf6f1bb7141133a8c49d06b3bc205775f7326
amirdev0 <amigoabdur@gmail.com>
2024-03-31 22:47:30 +0500

    Implement restaurant.h functions

--- src/client/restaurant/restaurant.c
+++ src/client/restaurant/restaurant.c
@@ -1,7 +1,66 @@
+#include <string.h>
+
 #include "restaurant.h"
 
-int meal_create(struct menu_t *menu, string name, int price, path image);
+struct menu_t {
+	struct meal_t {
+		int id;
+		string name;
+		int price;
+		path image;
+	} meal[MAXSIZE];
+};
 
-int meal_update(struct menu_t *menu, int meal_id, int price, path image);
+int meal_add(struct menu_t *menu, string name, int price, path image)
+{
+	int index = -1;
+	for (int i = 0; index < 0 && i < MAXSIZE; i++)
+		if (menu->meal[i].id == 0)
+			index = i;
 
-int meal_delete(struct menu_t *menu, int meal_id);
+	if (index < 0)
+		return -1;
+	
+	menu->meal[index].id = index;
+	strncpy(menu->meal[index].name, name, STRSIZE);
+	menu->meal[index].price = price;
+	strncpy(menu->meal[index].image, image, MAXSIZE);
+		
+	return 0;
+}
+
+int meal_modify(struct menu_t *menu, int meal_id, string name, int price, path image)
+{
+	int index = -1;
+	for (int i = 0; index < 0 && i < MAXSIZE; i++)
+		if (menu->meal[i].id == meal_id)
+			index = i;
+	
+	if (index < 0)
+		return -1;
+	
+	menu->meal[index].id = index;
+	strncpy(menu->meal[index].name, name, STRSIZE);
+	menu->meal[index].price = price;
+	strncpy(menu->meal[index].image, image, MAXSIZE);
+	
+	return 0;	
+}
+
+int meal_remove(struct menu_t *menu, int meal_id)
+{
+	int index = -1;
+	for (int i = 0; index < 0 && i < MAXSIZE; i++)
+		if (menu->meal[i].id == meal_id)
+			index = i;
+	
+	if (index < 0)
+		return -1;
+	
+	menu->meal[index].id = 0;
+	memset(menu->meal[index].name, '\0', STRSIZE);
+	menu->meal[index].price = 0;
+	memset(menu->meal[index].image, '\0', MAXSIZE);
+	
+	return 0;	
+}
--- src/client/restaurant/restaurant.h
+++ src/client/restaurant/restaurant.h
@@ -3,17 +3,12 @@
 
 #include "foodex_types.h"
 
-struct menu_t {
-	struct meal_t {
-		int id;
-		string name;
-		int price;
-		path image;
-	} meal[MAXSIZE];
-};
+struct menu_t;
 
-int meal_create(struct menu_t *menu, string name, int price, path image);
-int meal_update(struct menu_t *menu, int meal_id, int price, path image);
-int meal_delete(struct menu_t *menu, int meal_id);
+int meal_add(struct menu_t *menu, string name, int price, path image);
+
+int meal_modify(struct menu_t *menu, int meal_id, string name, int price, path image);
+
+int meal_remove(struct menu_t *menu, int meal_id);
 
 #endif //RESTAURANT_H
