478e55cc1161a8310117e088e76f3b1879458039
amirdev0 <amigoabdur@gmail.com>
2024-03-31 18:14:51 +0500

    Fix test_db.c

--- tests/test_db.c
+++ tests/test_db.c
@@ -1,38 +1,67 @@
-int main (int argc, char **argv)
+//gcc test_db.c -o test_db $(pkg-config --cflags --libs mysqlclient)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mysql.h>
+
+#define DB_HOST "localhost"
+#define DB_USER "root"
+#define DB_PASS "password"
+#define DB_NAME "FoodEx"
+
+int main(int argc, char **argv)
 {
-	MYSQL *con = foodle_db_init();
-	// How to get menu:
-	// struct foodle_meal_t *menu = foodle_db_get_menu(con, 239);
-	// struct foodle_meal_t *ptr;
-	// for (ptr = menu; ptr->ID != 0; ptr++) {
-	// 	printf("%d, %s, %f, %s\n", ptr->ID, ptr->name, ptr->price, ptr->image_path);
-	// }
-	// free(menu);
-	// mysql_close(con);
+	MYSQL *con = mysql_init(NULL);
+	if (con == NULL) {
+		fprintf(stderr, "ERR, Failed to initialize connection: %s\n", mysql_error(con));
+		exit(1);
+  	}
+  	printf("OK, Initialized connection\n");
+  	
+	if (mysql_real_connect(con, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0) == NULL) {
+		fprintf(stderr, "ERR, Failed to establish connection: %s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}
+  	printf("OK, Established connection to database\n");
+  	
+	char query[200] = { 0 };
+	sprintf(query, "SELECT * FROM user");
+	if (mysql_query(con, query)) {
+		fprintf(stderr, "ERR, Failed to execute query: %s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}
+	printf("OK, Queried database with statement\n");
+	
+	MYSQL_RES *res = mysql_store_result(con);
+	if (res == NULL) {
+		fprintf(stderr, "ERR, Failed to store result: %s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}
+	printf("OK, Stored result of query\n");
+	
+	printf("\nPrinting result of query:\n");
+	int num_fields = mysql_num_fields(res);
+	
+	MYSQL_FIELD *field;
+	while (field = mysql_fetch_field(res))
+		printf("%s\t", field->name);
+	printf("\n");
+  
+	MYSQL_ROW row;
+	while ((row = mysql_fetch_row(res)) != NULL) {
+		for (int i = 0; i < num_fields; i++)
+			printf("%s\t", row[i] ? row[i] : "NULL");
+		printf("\n");
+		break; //print only first row
+	}
+	
+	mysql_free_result(res);
+	
+	mysql_close(con);
 
-	struct foodle_meal_t meal = {
-		.ID = 414,
-		.name = "Pizza but changed",
-		.image_path = "Hello",
-		.price = 91.91,
-	};
-
-	// int r = foodle_db_update_meal_bystruct(con, &meal);
-	// struct foodle_meal_t newmeal = foodle_db_get_meal_byid(con, 414);
-	// printf("%d, %s, %f, %s\n", meal.ID, meal.name, meal.price, meal.image_path);
-	// printf("%d\n", r);
-	// printf("%d, %s, %f, %s\n", newmeal.ID, newmeal.name, newmeal.price, newmeal.image_path);
-
-	//int r = foodle_db_delete_meal_byid(con, 414);
-
-
-	// struct foodle_user_t *rests = foodle_db_get_restaurants(con);
-	// struct foodle_user_t *ptr;
-	// printf("kffk");
-	// for (ptr = rests; ptr->ID != 0; ptr++) {
-	// 	printf("'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'\n)", ptr->name, ptr->phone, ptr->email, ptr->password, ptr->address, ptr->region, ptr->image_path, ptr->delivery_type);
-	// }
-
-	exit(0);
+	return 0;
 }
-
